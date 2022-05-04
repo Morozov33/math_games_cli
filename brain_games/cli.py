ff81066e9076d1b4accf731a901480f008240013
Dmitrii <33morozov@gmail.com>
2022-05-04 11:01:29 +0300

    add new linter flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,10 +1,10 @@
-# '''Funchions for brain_games'''
+# """Funchions for brain_games"""
 
 import prompt
 
 
 def welcome_user():
-    '''Greeting new user'''
-    print ('Welcome to Brain Games!')
+    """Greeting new user"""
+    print('Welcome to Brain Games!')
     name_user = prompt.string('May I know your name?\n')
-    print (f'Nice to meet you, {name_user}!')
+    print(f'Nice to meet you, {name_user}!')
--- brain_games/scripts/brain_game.py
+++ brain_games/scripts/brain_game.py
@@ -1,12 +1,13 @@
-# Greeting script for brain_games
-
 #!/urs/bin/env python
 
+# Greeting script for brain_games
+
 from brain_games.cli import welcome_user
+
 
 def main():
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
